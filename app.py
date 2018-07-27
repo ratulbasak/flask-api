c210df076b9294869c1896b465f081b5901c5cc9
Ratul Basak <ratulbasak93@gmail.com>
2018-07-27 21:49:03 +0600

    commit

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def hello_world():
     return 'Flask Dockerized new...........'
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=8080)
+    app.run(debug=True,host='0.0.0.0',port=8080) 
